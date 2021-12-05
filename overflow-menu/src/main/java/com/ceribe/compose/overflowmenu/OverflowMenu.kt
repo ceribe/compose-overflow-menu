e770c8a6a0fa9c67c640a3ee79ff6862e86b8a6d
Ceribe <ceribekagami@gmail.com>
2021-12-05 17:32:48 +0100

    Simplify Overflow Menu implementation

--- overflow-menu/src/main/java/com/ceribe/compose/overflowmenu/OverflowMenu.kt
+++ overflow-menu/src/main/java/com/ceribe/compose/overflowmenu/OverflowMenu.kt
@@ -1,7 +1,6 @@
 package com.ceribe.compose.overflowmenu
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
@@ -18,55 +17,53 @@ fun OverflowMenu(params: OverflowMenuParamsDTO) {
                 listOf()
             else
                 icons.subList(shownIconsCount, icons.size)
-        Row {
-            shownIcons.forEach {
+        shownIcons.forEach {
+            IconButton(
+                onClick = it.onClick ?: defaults.onClick ?: {},
+                modifier = it.modifier ?: defaults.modifier ?: Modifier,
+                enabled = it.enabled ?: defaults.enabled ?: true
+            ) {
+                Icon(
+                    imageVector = it.imageVector ?: defaults.imageVector ?: Icons.Filled.Warning,
+                    contentDescription = it.contentDescription ?: defaults.contentDescription,
+                    tint = it.tint ?: defaults.tint ?: LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+                )
+            }
+        }
+        if(hiddenIcons.isNotEmpty()) {
+            Box {
+                var showDropDown by remember { mutableStateOf(false) }
                 IconButton(
-                    onClick = it.onClick ?: defaults.onClick ?: {},
-                    modifier = it.modifier ?: defaults.modifier ?: Modifier,
-                    enabled = it.enabled ?: defaults.enabled ?: true
+                    onClick = {
+                        showDropDown = true
+                        (overflowIcon.onClick ?: defaults.onClick ?: {})()
+                    },
+                    modifier = overflowIcon.modifier ?: defaults.modifier ?: Modifier,
+                    enabled = overflowIcon.enabled ?: defaults.enabled ?: true
                 ) {
                     Icon(
-                        imageVector = it.imageVector ?: defaults.imageVector ?: Icons.Filled.Warning,
-                        contentDescription = it.contentDescription ?: defaults.contentDescription,
-                        tint = it.tint ?: defaults.tint ?: LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+                        imageVector = overflowIcon.imageVector ?: defaults.imageVector
+                        ?: Icons.Filled.Warning,
+                        contentDescription = overflowIcon.contentDescription
+                            ?: defaults.contentDescription,
+                        tint = overflowIcon.tint ?: defaults.tint
+                        ?: LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
                     )
                 }
-            }
-            if(hiddenIcons.isNotEmpty()) {
-                Box {
-                    var showDropDown by remember { mutableStateOf(false) }
-                    IconButton(
-                        onClick = {
-                            showDropDown = true
-                            (overflowIcon.onClick ?: defaults.onClick ?: {})()
-                        },
-                        modifier = overflowIcon.modifier ?: defaults.modifier ?: Modifier,
-                        enabled = overflowIcon.enabled ?: defaults.enabled ?: true
-                    ) {
-                        Icon(
-                            imageVector = overflowIcon.imageVector ?: defaults.imageVector
-                            ?: Icons.Filled.Warning,
-                            contentDescription = overflowIcon.contentDescription
-                                ?: defaults.contentDescription,
-                            tint = overflowIcon.tint ?: defaults.tint
-                            ?: LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
-                        )
-                    }
-                    DropdownMenu(
-                        expanded = showDropDown,
-                        onDismissRequest = { showDropDown = false }
-                    ) {
-                        hiddenIcons.forEach {
-                            DropdownMenuItem(
-                                modifier = it.modifier ?: defaults.modifier ?: Modifier,
-                                enabled = it.enabled ?: defaults.enabled ?: true,
-                                onClick = {
-                                    showDropDown = false
-                                    (it.onClick ?: defaults.onClick ?: {})()
-                                }
-                            ) {
-                                Text(it.label ?: defaults.label ?: "")
+                DropdownMenu(
+                    expanded = showDropDown,
+                    onDismissRequest = { showDropDown = false }
+                ) {
+                    hiddenIcons.forEach {
+                        DropdownMenuItem(
+                            modifier = it.modifier ?: defaults.modifier ?: Modifier,
+                            enabled = it.enabled ?: defaults.enabled ?: true,
+                            onClick = {
+                                showDropDown = false
+                                (it.onClick ?: defaults.onClick ?: {})()
                             }
+                        ) {
+                            Text(it.label ?: defaults.label ?: "")
                         }
                     }
                 }
