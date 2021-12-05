8040e336d65e15aa7ee55911bdcae4a61de71600
Ceribe <ceribekagami@gmail.com>
2021-12-05 17:22:53 +0100

    Add more samples

--- sample/src/main/java/com/ceribe/compose/sample/MainActivity.kt
+++ sample/src/main/java/com/ceribe/compose/sample/MainActivity.kt
@@ -9,12 +9,12 @@ import androidx.compose.animation.*
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.List
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import com.ceribe.compose.overflowmenu.OverflowMenu
 import com.ceribe.compose.sample.ui.theme.SampleTheme
@@ -31,6 +31,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Sample1()
+//                    Sample2()
+//                    Sample3()
                 }
             }
         }
@@ -69,6 +71,75 @@ fun Sample1() {
                         }
                         overflowIcon {
                             tint = Color.Yellow
+                        }
+                    }
+                }
+            )
+        }
+    ) {}
+}
+
+@Composable
+fun Sample2() {
+    var numberOfShownIcons by remember { mutableStateOf(3) }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Sample 2") },
+                actions = {
+                    OverflowMenu {
+                        shownIconsCount = numberOfShownIcons
+                        icons {
+                            icon {
+                                onClick = { numberOfShownIcons = 3 }
+                                tint = Color.Green
+                                imageVector = Icons.Filled.Add
+                            }
+                            icon {
+                                imageVector = Icons.Filled.Settings
+                            }
+                            icon {
+                                onClick = { numberOfShownIcons = 1 }
+                                tint = Color.Red
+                                imageVector = Icons.Filled.Delete
+                            }
+                            icon {
+                                label = "Fourth Example"
+                            }
+                            icon {
+                                label = "Fifth Example"
+                            }
+                        }
+                        defaults {
+                            label = "Default Label"
+                        }
+                    }
+                }
+            )
+        }
+    ) {}
+}
+
+@Composable
+fun Sample3() {
+    val context = LocalContext.current
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Sample 3") },
+                actions = {
+                    OverflowMenu {
+                        shownIconsCount = 1
+                        icons {
+                            icon {
+                                onClick = { context.toast("Clicked") }
+                                modifier = Modifier.rotate(30f)
+                                enabled = true
+                                imageVector = Icons.Filled.Favorite
+                                label = "Example label"
+                                contentDescription = "Sample Icon"
+                                tint = Color.Cyan
+                            }
                         }
                     }
                 }
